7d8bcec1f68ecd6f3fe574661d8c2becf5a2b8c6
GENTAXXX <bramantyaraka00@mail.ugm.ac.id>
2020-05-06 16:45:01 +0700

    First Commit

---
+++ resources/views/cekresi.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+
+@section('title')
+Cek Resi
+@endsection
+
+@section('konten')
+<div class="s01">
+    <form>
+        <fieldset>
+            <legend>Cek Ongkos Kirim</legend>
+        </fieldset>
+        <div class="inner-form">
+            <div class="input-field first-wrap">
+                <input id="search" type="text" placeholder="Masukan Resi" />
+            </div>
+            <div class="input-field third-wrap">
+                <button class="btn-search" type="button">Search</button>
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+@extends('layout')
+
+@section('title')
+Cek Ongkos Kirim
+@endsection
+
+@section('konten')
+<div class="s01">
+    <form>
+        <fieldset>
+            <legend>Cek Ongkos Kirim</legend>
+        </fieldset>
+        <div class="inner-form">
+            <div class="input-field first-wrap">
+                <input id="search" type="text" placeholder="Dari mana" />
+            </div>
+            <div class="input-field second-wrap">
+                <input id="location" type="text" placeholder="Kemana" />
+            </div>
+            <div class="input-field third-wrap">
+                <button class="btn-search" type="button">Search</button>
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
